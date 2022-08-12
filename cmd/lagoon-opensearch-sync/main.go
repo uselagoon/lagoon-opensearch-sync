050b4ecf88e04edd949f872bc33e387f1f9d7afa
Scott Leggett <scott@sl.id.au>
2022-08-12 16:23:59 +0800

    fix: register dump-roles command

--- cmd/lagoon-opensearch-sync/main.go
+++ cmd/lagoon-opensearch-sync/main.go
@@ -18,7 +18,8 @@ type CLI struct {
 	Debug        bool            `kong:"env='DEBUG',help='Enable debug logging'"`
 	Version      VersionCmd      `kong:"cmd,help='Print version information'"`
 	DumpProjects DumpProjectsCmd `kong:"cmd,help='Print Lagoon Projects JSON to standard out'"`
-	DumpGroups   DumpGroupsCmd   `kong:"cmd,help='Print Lagoon Groups JSON to standard out'"`
+	DumpGroups   DumpGroupsCmd   `kong:"cmd,help='Print Keycloak Groups JSON to standard out'"`
+	DumpRoles    DumpRolesCmd    `kong:"cmd,help='Print Opensearch Roles JSON to standard out'"`
 }
 
 func main() {
