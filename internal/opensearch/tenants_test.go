970feaa7d8ce16ee346e9effc6a198c49c41d4c9
Scott Leggett <scott@sl.id.au>
2022-08-16 11:29:03 +0800

    chore: add tests for dump-tenants

---
+++ internal/opensearch/tenants_test.go
@@ -0,0 +1,64 @@
+package opensearch_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/uselagoon/lagoon-opensearch-sync/internal/opensearch"
+)
+
+func TestTenantsUnmarshal(t *testing.T) {
+	var testCases = map[string]struct {
+		input  string
+		expect map[string]opensearch.Tenant
+	}{
+		"unmarshal tenants": {
+			input: "testdata/tenants.json",
+			expect: map[string]opensearch.Tenant{
+				"admin_tenant": {
+					Description: "Tenant for admin user",
+				},
+				"amazee.io internal": {
+					Description: "amazee.io internal",
+				},
+				"drupal-example": {
+					Description: "drupal-example",
+				},
+				"global_tenant": {
+					Description: "Global tenant",
+					Reserved:    true,
+					Static:      true,
+				},
+				"internaltest": {
+					Description: "internaltest",
+				},
+				"lagoonadmin": {
+					Description: "lagoonadmin",
+				},
+			},
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(tt *testing.T) {
+			data, err := os.ReadFile(tc.input)
+			if err != nil {
+				tt.Fatal(err)
+			}
+			// check for missing fields in opensearch.Tenant
+			var tenants map[string]opensearch.Tenant
+			decoder := json.NewDecoder(bytes.NewReader(data))
+			decoder.DisallowUnknownFields()
+			if err = decoder.Decode(&tenants); err != nil {
+				tt.Fatal(err)
+			}
+			if !reflect.DeepEqual(tc.expect, tenants) {
+				tt.Fatalf("expected:\n%s\ngot\n%s\n",
+					spew.Sdump(tc.expect), spew.Sdump(tenants))
+			}
+		})
+	}
+}
